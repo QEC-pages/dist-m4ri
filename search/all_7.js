e8b5d3cb1119f4e4fa8d3dd8a874350688a28f0e
LeonidPryadko <LeonidPryadko@users.noreply.github.com>
2025-08-03 21:02:56 +0000

    Deploying to gh-pages from @ QEC-pages/dist-m4ri@d08b47f8d958f9b861193213f1658f82c70383fe 🚀

--- search/all_7.js
+++ search/all_7.js
@@ -17,10 +17,11 @@ var searchData=
   ['distance_14',['operation mode: find upper bound on the distance.',['../md_todo.html#autotoc_md19',1,'']]],
   ['distance_20of_20a_20classical_20or_20quantum_20css_20code_15',['dist-m4ri - distance of a classical or quantum CSS code',['..//home/runner/work/dist-m4ri/dist-m4ri/README.md#autotoc_md11',1,'']]],
   ['distance_20verification_16',['operation mode: distance verification.',['../md_todo.html#autotoc_md18',1,'']]],
-  ['do_20i_20make_20a_20contribution_17',['How do I make a contribution?',['../md_CONTRIBUTING.html#autotoc_md2',1,'']]],
-  ['do_5fcc_5fdist_18',['do_CC_dist',['../dist__cc_8c.html#ab67a3351d0d1c6c20e829cac3e8e5374',1,'dist_cc.c']]],
-  ['do_5fdist_5fclus_19',['do_dist_clus',['../util__m4ri_8h.html#a63e16fae6732551ceefdb0cee6238b27',1,'util_m4ri.h']]],
-  ['do_5freduce_20',['do_reduce',['../util__m4ri_8h.html#a28bb122363dc1e068f508ebf67da5df7',1,'do_reduce(mzd_t *row, const mzd_t *matP0, const rci_t rankP0):&#160;util_m4ri.c'],['../util__m4ri_8c.html#aa544a382a89cfb255aed9149d687ee08',1,'do_reduce(mzd_t *row, const mzd_t *matG0, const rci_t rankG0):&#160;util_m4ri.c']]],
-  ['do_5frw_5fdist_21',['do_RW_dist',['../dist__m4ri_8c.html#aecc4236307ca7b8cc9b13a52b13e2dbb',1,'dist_m4ri.c']]],
-  ['documentation_22',['Documentation',['../index.html#autotoc_md8',1,'']]]
+  ['dmax_17',['dmax',['../structparams__t.html#a763ac3ea80f8ea38c4ba670cf256aa38',1,'params_t']]],
+  ['do_20i_20make_20a_20contribution_18',['How do I make a contribution?',['../md_CONTRIBUTING.html#autotoc_md2',1,'']]],
+  ['do_5fcc_5fdist_19',['do_CC_dist',['../dist__cc_8c.html#ab67a3351d0d1c6c20e829cac3e8e5374',1,'dist_cc.c']]],
+  ['do_5fdist_5fclus_20',['do_dist_clus',['../util__m4ri_8h.html#a63e16fae6732551ceefdb0cee6238b27',1,'util_m4ri.h']]],
+  ['do_5freduce_21',['do_reduce',['../util__m4ri_8h.html#a28bb122363dc1e068f508ebf67da5df7',1,'do_reduce(mzd_t *row, const mzd_t *matP0, const rci_t rankP0):&#160;util_m4ri.c'],['../util__m4ri_8c.html#aa544a382a89cfb255aed9149d687ee08',1,'do_reduce(mzd_t *row, const mzd_t *matG0, const rci_t rankG0):&#160;util_m4ri.c']]],
+  ['do_5frw_5fdist_22',['do_RW_dist',['../dist__m4ri_8c.html#aecc4236307ca7b8cc9b13a52b13e2dbb',1,'dist_m4ri.c']]],
+  ['documentation_23',['Documentation',['../index.html#autotoc_md8',1,'']]]
 ];
--- search/variables_3.js
+++ search/variables_3.js
@@ -3,5 +3,6 @@ var searchData=
   ['debug_0',['debug',['../structparams__t.html#ac71d44285373a5bfe9aec4bb8b3a8bea',1,'params_t']]],
   ['dist_1',['dist',['../structparams__t.html#add35140e341aba1ab36244a70be29393',1,'params_t']]],
   ['dist_5fmax_2',['dist_max',['../structparams__t.html#a23a033fd80e49ab1d7761c05a832c248',1,'params_t']]],
-  ['dist_5fmin_3',['dist_min',['../structparams__t.html#a668de621912ff858fe5a235b8b2c5345',1,'params_t']]]
+  ['dist_5fmin_3',['dist_min',['../structparams__t.html#a668de621912ff858fe5a235b8b2c5345',1,'params_t']]],
+  ['dmax_4',['dmax',['../structparams__t.html#a763ac3ea80f8ea38c4ba670cf256aa38',1,'params_t']]]
 ];
