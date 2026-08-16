6cf17331dcb8e739b6d981bf5d2ad7f99db9853a
LeonidPryadko <63686094+LeonidPryadko@users.noreply.github.com>
2026-08-16 01:05:55 +0000

    Deploying to gh-pages from @ QEC-pages/dist-m4ri@b403ee9a25904578cc61392cd1fd177ce59dcdc9 🚀

--- search/variables_4.js
+++ search/variables_4.js
@@ -7,6 +7,6 @@ var searchData=
   ['dist_5fmax_4',['dist_max',['../structparams__t.html#a23a033fd80e49ab1d7761c05a832c248',1,'params_t']]],
   ['dist_5fmin_5',['dist_min',['../structparams__t.html#a668de621912ff858fe5a235b8b2c5345',1,'params_t']]],
   ['dmax_6',['dmax',['../structdistfork__ctx__t.html#af98b8c2a0518e7a6fd1baa3247ac17d5',1,'distfork_ctx_t::dmax'],['../structparams__t.html#a763ac3ea80f8ea38c4ba670cf256aa38',1,'params_t::dmax']]],
-  ['dmin_7',['dmin',['../structdistfork__ctx__t.html#a232e1110afae4c44fe1bbec314d44cf9',1,'distfork_ctx_t']]],
+  ['dmin_7',['dmin',['../structdistfork__ctx__t.html#a232e1110afae4c44fe1bbec314d44cf9',1,'distfork_ctx_t::dmin'],['../structparams__t.html#a1cdd33ce7e158f0542ada5a1cc86cc2b',1,'params_t::dmin']]],
   ['dw_8',['dW',['../structparams__t.html#aac4a3fa59ee29835b0914c6e48eb13f0',1,'params_t']]]
 ];
